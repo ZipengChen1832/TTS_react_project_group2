665ee810daafc9720a32d9a3ffc5928e66b03cbb
Isaacson Michel <84110054+isaac-michel224@users.noreply.github.com>
2021-09-13 00:16:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, {useState} from 'react';
-import Tweet from './components/tweet';
 
-const App = () => {
-    return(
-        <Tweet />
-    )
+import Login from "./components/Login";
+import Tweet from "./components/tweet";
+import { BrowserRouter as Switch, Route, Router } from 'react-router-dom';
+
+
+
+function App() {
+  return(
+
+    <Router>
+    <Switch>
+
+<Route exact path="/login" component={Login} />
+
+<Route exact path="/tweet" component={Tweet} />
+
+    </Switch>
+</Router>
+
+  );
 }
 
-
-
 export default App;
+
+
